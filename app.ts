7b7a021c63b74b809f4f877613137b6cd4771f08
Kennedy Mahoro <kennedymahoro1@gmail.com>
2022-06-07 15:27:47 +0000

    added typescript

--- app.ts
+++ app.ts
@@ -1,48 +1,49 @@
 // query selectors
-const Numbers:NodeListOf<HTMLButtonElement> = document.querySelectorAll("[data-number]")
-const displayOutput = document.querySelector("[data-display-output]")
-const operators = document.querySelectorAll("[data-operator]")
-const deleteBtn = document.querySelector("[data-delete]")
-const clearBtn = document.querySelector("[data-clear]")
-const percentBtn = document.querySelector("[data-percent]")
-const equalsBtn = document.querySelector("[data-equals]")
+const numbers:NodeListOf<HTMLButtonElement> = document.querySelectorAll("[data-number]")
+const displayOutput:HTMLDivElement = document.querySelector("[data-display-output]")
+const operators:NodeListOf<HTMLButtonElement> = document.querySelectorAll("[data-operator]")
+const deleteBtn:HTMLButtonElement = document.querySelector("[data-delete]")
+const clearBtn:HTMLButtonElement= document.querySelector("[data-clear]")
+const percentBtn:HTMLButtonElement = document.querySelector("[data-percent]")
+const equalsBtn:HTMLButtonElement = document.querySelector("[data-equals]")
 // variables
-let AllOutput = [];
-let ArrayOfOutputNum = [];// array of outputed numbers
-let Pre_OutputNum;// previous output number
+let AllOutput:any = [];
+let Pre_OutputNum:number;// previous output number
 
 // operator symbol
-const Addition_Symbol = operators[0].innerText;
-const subtraction_Symbol = operators[1].innerText;
-const multiplication_Symbol = operators[2].innerText;
-const divition_Symbol = operators[3].innerText;
+const Addition_Symbol:string = operators[0].innerText;
+const subtraction_Symbol:string = operators[1].innerText;
+const multiplication_Symbol:string = operators[2].innerText;
+const divition_Symbol:string = operators[3].innerText;
 
 
 // forEach number
 numbers.forEach(number => {
-    number.addEventListener("click", ()=>{
-        //displayOutput.innerHTML =  inputs(number.innerText,null);
+    number.addEventListener("click", ():void=>{
         AllOutput.push(number.innerText)
         displayOutput.innerHTML = AllOutput.join('') 
     })
 })
 operators.forEach(operator => {
-    operator.addEventListener('click', ()=>{
+    operator.addEventListener('click', ():void=>{
         AllOutput.push(operator.innerText)
         displayOutput.innerHTML = AllOutput.join('') 
     })
 })
-clearBtn.addEventListener('click', ()=>{
-    Pre_OutputNum = displayOutput.innerText;
+clearBtn.addEventListener('click', ():void=>{
     displayOutput.innerHTML = null;
     AllOutput = []
 })
-deleteBtn.addEventListener('click', ()=>{
-    ArrayOfOutputNum.pop()
+deleteBtn.addEventListener('click', ():void=>{
     AllOutput.pop()
     displayOutput.innerHTML = AllOutput.join('') 
 
 })
-equalsBtn.addEventListener('click',()=>{
+equalsBtn.addEventListener('click',():void=>{
     let SplitOutputArray = AllOutput.join('').split("")
+
+    
+    
+    
+    Pre_OutputNum = Number(displayOutput.innerText);
 })
